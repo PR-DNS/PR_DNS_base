9c26f35ac693adc078af8cb6cbc519c79a31c474
Tao Zhang <taozhang.ccs@gmail.com>
2025-03-05 15:58:04 -0800

    heffte, and output of each time step

--- DNS/climate/run_DNS_3d.sh
+++ DNS/climate/run_DNS_3d.sh
@@ -1,20 +1,16 @@
-#PBS -q batch
-#PBS -m abe
-#PBS -l nodes=2:ppn=32
-#PBS -l walltime=960:00:00
-#PBS -j oe
-#PBS -N dns_3d
-#PBS -o /home/tzhang/tmp/$PBS_JOBID.out
-##PBS -M tzhang@bnl.gov
-#PBS -n
+#!/bin/bash
+#SBATCH -q debug
+#SBATCH -A m4402
+#SBATCH -J dns
+
+#SBATCH  --nodes=1
+#SBATCH  --output=../../../PR_DNS_out/debug.%j
+#SBATCH  --error=../../../PR_DNS_out/error.%j
+#SBATCH  --exclusive
+#SBATCH  --time=0:30:00
+#SBATCH  --constraint=cpu
 
 ulimit -s unlimited
-#export OMP_STACKSIZE=2000M
-#export MP_STACK_SIZE=2000M
-#export OMP_NUM_THREADS=1
 
-export LD_LIBRARY_PATH="/home/tzhang/soft/jpeg-9c/lib/":$LD_LIBRARY_PATH
-
-cd /home/tzhang/PR_DNS_base/DNS/climate/
-rm -rf out-entrainment3dd_case1
-/home/tzhang/soft/openmpi-4.1.1/bin/mpirun --mca  btl_openib_allow_ib 1  -np 64 /home/tzhang/PR_DNS_base/DNS/climate/climate -d 3 -p 4 4 4 -i input-pr-dns/in-entrainment3dd_case1 -o out-entrainment3dd_case1
+#cd /sdcc/u/tzhang/PR_DNS_base/DNS/climate
+srun -n 128  /pscratch/sd/z/zhangtao/PR_DNS/PR_DNS_heffte/PR_DNS_base/DNS/climate/climate -d 3 -p 8 4 4  -i input-pr-dns/in-entrainment3dd_case3_abdullah  -o /pscratch/sd/z/zhangtao/PR_DNS/PR_DNS_heffte/PR_DNS_base/DNS/climate/out-entrainment3dd_case3
